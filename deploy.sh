469af51a6ad586e701ee3de24ebd987833df7de6
Fabian Gietzen <fabian.gietzen@web.de>
2023-04-02 14:45:08 +0200

    Build tar archive on release

--- deploy.sh
+++ deploy.sh
@@ -16,18 +16,26 @@ if test ! "$(echo $version | grep -E -o '[0-9]+\.[0-9]+\.[0-9]+')"; then
 	exit 1
 fi
 
+set_version $version
+
 echo "Update dependencies (Cargo.lock)"
 cargo update
 git add Cargo.lock
 git commit -m "Update dependencies"
 
-set_version $version
+echo "Tag new release"
 git add Cargo.toml
 git commit -m "Release of version $version"
 git tag "$version"
 
+echo "Build tar archive"
+mkdir -p releases
+tar -czf "releases/dockerserver-$version.tar.gz" src/ Cargo.lock Cargo.toml dockerfile
+
+echo "Set next dev version"
 readarray -d "." -t v <<< $version
 next_version="${v[0]}.${v[1]}.$(expr ${v[2]} + 1)-dev"
 set_version $next_version
-git add Cargo.toml
+cargo update
+git add Cargo.toml Cargo.lock
 git commit -m "Next dev version"
